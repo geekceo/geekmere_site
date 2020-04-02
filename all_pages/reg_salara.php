93ee471e78372b2b9d956a2c60b156e1ee5ee45a
Tagir <khalilov.tg@gmail.com>
2020-04-02 18:55:02 +0300

    Adaptive screen size and new beta page with SalaraCloud's beta-test

--- all_pages/reg_salara.php
+++ all_pages/reg_salara.php
@@ -2,7 +2,7 @@
  
 $sendto   = "gmstudio.classic@gmail.com"; // почта, на которую будет приходить письмо
 $userpass = $_POST['pass'];
-$usermail = $_POST['mail'];
+$usermail = $_POST['email'];
  
 // Формирование заголовка письма
 $subject  = "Участие в бета-тесте SalaraCloud";
@@ -21,6 +21,22 @@ $msg .= "</body></html>";
  
 // отправка сообщения
 if(@mail($sendto, $subject, $msg, $headers)) {
+	$sendto   = $usermail;
+	// Формирование заголовка письма
+	$subject  = "Участие в бета-тесте SalaraCloud";
+	$headers  = "From: " . strip_tags("gmstudio.classic@gmail.com") . "\r\n";
+	$headers .= "Reply-To: ". strip_tags("gmstudio.classic@gmail.com") . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+ 
+	// Формирование тела письма
+	$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
+	$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Команда GeekMere Inc.</h2>\r\n";
+	$msg .= "<p> Благодарим вас за участие в бета-тесте.</p>\r\n";
+	$msg .= "<p> В скором времени вам будет выслана копия продукта для тестирования</p>\r\n";
+	$msg .= "<p><strong>С уважением, GeekMere Inc.</strong></p>\r\n";
+	$msg .= "</body></html>";
+	@mail($sendto, $subject, $msg, $headers)
     header ('Location: /all_pages/beta-salara.html');  // перенаправление на нужную страницу
     exit();    // прерываем работу скрипта, чтобы забыл о прошлом
 } else {
